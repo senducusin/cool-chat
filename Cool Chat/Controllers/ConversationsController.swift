1d82c820e9c2cbbcbbe8ce6c5181c78e88335e1e
Jansen Ducusin <senducusin@gmail.com>
2021-03-12 17:03:23 +0800

    Removed default scenes and VC

--- Cool Chat/Controllers/ConversationsController.swift
+++ Cool Chat/Controllers/ConversationsController.swift
@@ -11,12 +11,25 @@ class ConversationsController: UIViewController {
     // MARK: - Properties
     
     // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupUI()
     }
+    
+    // MARK: - Selectors
+    @objc private func showProfileDidTap(){
+        print("tapped!")
+    }
+    
     // MARK: - Helpers
+    
     private func setupUI(){
-        self.view.backgroundColor = .green
+        self.view.backgroundColor = .white
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.title = "Messages"
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image:image, style: .plain, target: self, action: #selector(showProfileDidTap))
     }
 }
