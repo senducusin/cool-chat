5e40b061b9448585ad9e1db5c61ba6e543191f61
Jansen Ducusin <senducusin@gmail.com>
2021-03-12 16:39:01 +0800

    Initial commit

---
+++ Cool Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cool Chat
+//
+//  Created by Jansen Ducusin on 3/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
