13d41145d8e46e75092a616710663d8f827d67dc
Jansen Ducusin <senducusin@gmail.com>
2021-03-12 17:49:12 +0800

    Setup tableview

--- Cool Chat/Controllers/ConversationsController.swift
+++ Cool Chat/Controllers/ConversationsController.swift
@@ -16,13 +16,12 @@ class ConversationsController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupUI()
+        self.setupTableView()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
         self.tableView.frame = self.view.bounds
-        
     }
     
     // MARK: - Selectors
@@ -32,6 +31,11 @@ class ConversationsController: UIViewController {
     
     // MARK: - Helpers
     
+    private func setupTableView(){
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
     private func setupUI(){
         self.view.backgroundColor = .white
         self.navigationController?.navigationBar.prefersLargeTitles = true
@@ -40,5 +44,22 @@ class ConversationsController: UIViewController {
         self.view.addSubview(self.tableView)
         
         self.navigationItem.leftBarButtonItem = UIBarButtonItem.createProfileButton(target: self, selector: #selector(showProfileDidTap))
+    }
+}
+
+// MARK: - TableView Datasource and Delegate
+extension ConversationsController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: UITableViewCell.conversationTVCellIdentifier, for: indexPath)
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
     }
 }
